20e87bc59c745b43fdcdb1949d480ff059f2d6b9
MaksymTovstolis <85698971+MaksymTovstolis@users.noreply.github.com>
2021-09-09 16:20:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+debugger;
+
+let action ;
+let countOfNumbers = +prompt('let your count of numbers')
+let result = 0;
+let finalResult = 1;
+
+
+do {
+     action =  prompt('let your action (+ - * / )') .trim() ;
+} while( !(action == "+" || action == "-"|| action == "*" || action == "/"));
+
+for  (i = 0; i < 7 && i < countOfNumbers; i++) { 
+    result += parseInt(prompt(`enter your value`));
+  }
+
+  if (action == "+") {
+    finalResult = 0;
+    finalResult += result;
+    }
+  if (action == "-"){
+    finalResult = 0;
+    finalResult -= result;
+  }
+  if (action == "/"){
+    finalResult = 1;
+    result /= finalResult;
+    finalResult = result;
+  }
+
+  if (action == "*"){
+    finalResult = 1;
+    finalResult *= result;
+  }
+
+console.log(action);
+console.log(countOfNumbers);
+console.log(result);
+console.log(finalResult);
